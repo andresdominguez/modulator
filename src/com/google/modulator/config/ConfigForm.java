0f6e2071e8a0a78f58d8c19ba4c889bd4dfcab0c
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-09-12 21:29:10 -0400

    add for skeleton

---
+++ src/com/google/modulator/config/ConfigForm.java
@@ -0,0 +1,9 @@
+package com.google.modulator.config;
+
+import javax.swing.*;
+
+public class ConfigForm {
+  private JRadioButton searchFilesRadioButton;
+  private JRadioButton useFileRadioButton;
+  private JRadioButton bothRadioButton;
+}
