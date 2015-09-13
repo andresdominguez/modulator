3ec45a32c9a186b14d4287fc908cb0e636f78b88
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-09-13 12:47:49 -0400

    new find result

--- src/com/google/modulator/FindResult.java
+++ src/com/google/modulator/FindResult.java
@@ -1,4 +1,13 @@
 package com.google.modulator;
 
+import com.intellij.openapi.vfs.VirtualFile;
+
 public class FindResult {
+  public final String token;
+  public final VirtualFile virtualFile;
+
+  public FindResult(String token, VirtualFile virtualFile) {
+    this.token = token;
+    this.virtualFile = virtualFile;
+  }
 }
