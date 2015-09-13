825b2a02fa8603e9bd9a90e75e3ff99a5db603e4
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-09-12 20:49:05 -0400

    formatting

---
+++ src/com/google/modulator/modules/ModuleCompletionProvider.java
@@ -0,0 +1,31 @@
+package com.google.modulator.modules;
+
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionProvider;
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+import static com.google.modulator.CompletionHelper.getCompletions;
+
+class ModuleCompletionProvider extends CompletionProvider<CompletionParameters> {
+  @Override
+  protected void addCompletions(@NotNull CompletionParameters completionParameters,
+                                ProcessingContext processingContext,
+                                @NotNull CompletionResultSet completionResultSet) {
+
+    PsiElement originalPosition = completionParameters.getOriginalPosition();
+    if (originalPosition == null) {
+      return;
+    }
+
+    String text = originalPosition.getParent().getParent().getParent().getText();
+    if (text == null || !text.startsWith("goog.require")) {
+      return;
+    }
+
+    final ModuleFinder moduleFinder = new ModuleFinder(originalPosition.getProject());
+    completionResultSet.addAllElements(getCompletions(moduleFinder.findModules()));
+  }
+}
